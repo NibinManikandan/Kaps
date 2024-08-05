266cb8339e78e8d71e4e181b3d3297cf83d5bb2f
Nibin M <nibin2319@gmail.com>
2024-08-05 19:41:37 +0530

    kaps_commit

--- KapsProject/settings.py
+++ KapsProject/settings.py
@@ -69,14 +69,24 @@ WSGI_APPLICATION = 'KapsProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kaps_cars',
+        'USER': 'postgres',
+        'PASSWORD': '1999',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
-
 
 
 REST_FRAMEWORK = {
